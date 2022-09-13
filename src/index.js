ea5e24f1627942b5e9240b1e59d290a522a23f9b
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-09-13 16:04:05 -0300

    added githubpages SPA setup

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/PostManager">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
