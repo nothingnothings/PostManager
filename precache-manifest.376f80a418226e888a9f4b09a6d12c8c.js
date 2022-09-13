db16d85402221c645ed235a48065b6920c3f9192
nothingnothings <nothingnothings@users.noreply.github.com>
2022-09-13 19:19:22 +0000

    deploy: 58566be835dc3ae0574c7cca5418843a10441e18

---
+++ precache-manifest.376f80a418226e888a9f4b09a6d12c8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d36165dddbccdf4ab2c7",
+    "url": "/PostManager/static/css/main.176757f9.chunk.css"
+  },
+  {
+    "revision": "d36165dddbccdf4ab2c7",
+    "url": "/PostManager/static/js/main.d36165dd.chunk.js"
+  },
+  {
+    "revision": "ab771bf3b2ba1cfcf1a7",
+    "url": "/PostManager/static/css/1.b0103ddb.chunk.css"
+  },
+  {
+    "revision": "ab771bf3b2ba1cfcf1a7",
+    "url": "/PostManager/static/js/1.ab771bf3.chunk.js"
+  },
+  {
+    "revision": "720003f3f19d5be24c3d",
+    "url": "/PostManager/static/js/runtime~main.720003f3.js"
+  },
+  {
+    "revision": "92790bd477514a9ba2c79b0bf0965bed",
+    "url": "/PostManager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af5664933890875d8147c94d76e3b49d.js"
+  "/PostManager/precache-manifest.376f80a418226e888a9f4b09a6d12c8c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/PostManager/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
